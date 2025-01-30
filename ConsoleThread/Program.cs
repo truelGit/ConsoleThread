9fcde0de18d01cecbba93272957110336b7102f2
truelGit <ivichev@gmail.com>
2025-01-30 19:06:05 +0100

    Create Project with Helper to run 6 threads to see the problem

---
+++ ConsoleThread/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleThread.Helpers;
+
+namespace ConsoleThread;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var threadHelper = new ThreadHelper();
+        threadHelper.Start();
+
+        Console.ReadKey();
+    }
+}
